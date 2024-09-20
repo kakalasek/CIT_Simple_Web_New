2164a14e716d8c8fdce8361efbb6f63b18b79923
Kakalasek <honziccekkj@seznam.cz>
2024-09-20 14:43:18 -0400

    basic flask layout

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 import os
+from wtforms import Form, StringField, SubmitField, validators
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.environ["DATABASE_URI"]}"
+#app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{os.environ["DATABASE_URI"]}"
 
 class Base(DeclarativeBase):
     pass
 
+class BlogForm(Form):
+    user = StringField("User", [validators.DataRequired()])
+    text = StringField("Text", [validators.DataRequired()])
+    submit = SubmitField("Send")
+
 db = SQLAlchemy(model_class=Base)
 
-class Comment(db.Model):
-    id: Mapped  
-    user = db.Column()
+@app.route("/")
+def default():
+    return redirect(url_for('page')), 302
 
 @app.route("/get-blogs")
 def get_blogs():
@@ -22,3 +28,7 @@ def get_blogs():
 @app.route("/post-blogs", methods=["POST"])
 def post_blogs():
     return "Not implemented yet", 400
+
+@app.route("/page", methods=["GET", "POST"])
+def page():
+    return render_template("page.html"), 200
